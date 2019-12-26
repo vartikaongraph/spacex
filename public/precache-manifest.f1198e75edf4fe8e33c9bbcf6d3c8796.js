f49a4b6a2a2e26861a2d852c118c05259396e37f
vartika <vartika.ongraph@gmail.com>
2019-12-26 16:00:19 +0530

    final

---
+++ public/precache-manifest.f1198e75edf4fe8e33c9bbcf6d3c8796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f562b87543eed2286a0c6a2d9fb61b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5def80e0075a37654ce",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "381c90223184e8dbab77",
+    "url": "/static/js/2.b8dddc15.chunk.js"
+  },
+  {
+    "revision": "b20a9500142ee9432972a75827d2f9fd",
+    "url": "/static/js/2.b8dddc15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5def80e0075a37654ce",
+    "url": "/static/js/main.42bae204.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
